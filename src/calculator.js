3823f11b1267523eba189c1be3513eb5496721f9
sarah <sarahkharraz@gmail.com>
2019-05-29 16:44:10 +0100

    Select a number from the menu and return it

--- src/calculator.js
+++ src/calculator.js
@@ -17,6 +17,56 @@ function start(){
   // Display the result
 }
 
+function ask(question, format, callback) {
+ let stdin = process.stdin, stdout = process.stdout;
+
+ stdin.resume();
+ stdout.write(question + ": ");
+
+ stdin.once('data', function(data) {
+   data = data.toString().trim();
+
+   if (format.test(data)) {
+     callback(data);
+   } else {
+     stdout.write("It should match: "+ format +"\n");
+     ask(question, format, callback);
+   }
+ });
+}
+ask("Number", /[1-4]/, function(number) {
+    console.log("The operation you want to do is: ", number);
+
+    process.exit();
+});
+
+function select(value){
+  let answer = ""
+
+  switch (value) {
+    case 1:
+    answer = "1"
+    // return sum()
+    break;
+
+    case 2:
+    answer = "2"
+    // return substract()
+    break;
+
+    case 3:
+    answer = "3"
+    // return divide()
+    break;
+
+    case 4:
+    answer = "4"
+    // return multiply()
+    break;
+  }
+  return answer
+}
+
 function number(){
   console.log("Please give us the numbers:")
 }
