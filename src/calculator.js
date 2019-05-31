bfe5de1012433559cb8b4d16e9d506c0631adfc6
sarahyjja <sarahkharraz@gmail.com>
2019-05-31 17:54:29 +0100

    Getting around Asynchronous function

--- src/calculator.js
+++ src/calculator.js
@@ -1,78 +1,38 @@
 const { sum, substract, divide, multiply } = require('./operation');
 
 const menu = `
-  This is a calculator.
+This is a calculator.
 
-  Please select an option:
-  1: Sum
-  2: Substract
-  3: Divide
-  4: Multiply
+Please select an option:
+1: Sum
+2: Substract
+3: Divide
+4: Multiply
 `
 
 function start(){
   console.log(menu);
-  // TODO read the option
-  // Do something based on the option
-  // Display the result
 }
 
-function ask(question, format, callback) {
- let stdin = process.stdin, stdout = process.stdout;
-
- stdin.resume();
- stdout.write(question + ": ");
-
- stdin.once('data', function(data) {
-   data = data.toString().trim();
-
-   if (format.test(data)) {
-     callback(data);
-   } else {
-     stdout.write("It should match: "+ format +"\n");
-     ask(question, format, callback);
-   }
- });
-}
-ask("Number", /[1-4]/, function(number) {
-    console.log("The operation you want to do is: ", number);
-
-    process.exit();
-});
-
-function select(value){
-  let answer = ""
-
-  switch (value) {
-    case 1:
-    answer = "1"
-    // return sum()
-    break;
-
-    case 2:
-    answer = "2"
-    // return substract()
-    break;
-
-    case 3:
-    answer = "3"
-    // return divide()
-    break;
-
-    case 4:
-    answer = "4"
-    // return multiply()
-    break;
-  }
-  return answer
+function userInput() {
+  return new Promise((resolve, reject) => {
+    process.stdin.once('data', (data) => {
+      resolve(data)
+    })
+  })
 }
 
-function number(){
-  console.log("Please give us the numbers:")
+async function menuNumber(){
+  const chosenNumber = await userInput()
+  console.log(chosenNumber)
 }
 
-function result(result){
-  console.log(`The result is ${result}`)
-}
+async function askForNumbers(chosenNumber){
+  const userInput1 = await userInput(); // returns a string, you want integers
+  const userInput2 = await userInput();
 
-module.exports = { start };
+  if(chosenNumber == "1") return sum(userInput1, userInput2)
+  else if(chosenNumber == "2") return substract(userInput1, userInput2)
+  else if(chosenNumber == "3") return divide(userInput1, userInput2)
+  else if(chosenNumber == "4") return multiply(userInput1, userInput2)
+}
