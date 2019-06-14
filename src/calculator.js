e381a619842b9eb38357febe0f0df1a82ec7160f
sarah <sarahkharraz@gmail.com>
2019-06-14 15:52:31 +0100

    Working around improvements

--- src/calculator.js
+++ src/calculator.js
@@ -19,6 +19,9 @@ function userInput() {
     process.stdin.once('data', (data) => {
       resolve(data)
     })
+    // stdout.write(`It should match: 12354 \n`);
+    // stdout.write(`try again`);
+    // start(menu)
   })
 }
 
@@ -33,10 +36,15 @@ async function askForOperationNumbers(chosenNumber){
   const userInput2 = await userInput();
   //console.log(`The first number is: ${userInput1} The second number is: ${userInput2}`)
 
-  if(chosenNumber == "1") return console.log(`The result is`, sum(Number(userInput1), Number(userInput2)))
+  if(chosenNumber == "1")
+  return console.log(`The result is`, sum(Number(userInput1), Number(userInput2)))
   else if(chosenNumber == "2") return console.log(`The result is:`, substract(Number(userInput1), Number(userInput2)))
   else if(chosenNumber == "3") return console.log(`The result is:`, divide(Number(userInput1), Number(userInput2)))
   else if(chosenNumber == "4") return console.log(`The result is:`, multiply(Number(userInput1), Number(userInput2)))
+  else {
+    console.log(`It should match 1/2/3/4/`)
+  }
+    process.exit();
 }
 
 
